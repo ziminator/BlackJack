0a93e2f5e028753f8b7b6904cb032881e4520e9a
ziminator <aleksander.zimin@gmail.com>
2019-02-20 22:49:36 +0300

    BlackJack

--- main.rb
+++ main.rb
@@ -3,8 +3,10 @@ require_relative 'interface.rb'
 require_relative 'player.rb'
 require_relative 'bank.rb'
 require_relative 'computer.rb'
+require_relative 'desk.rb'
 
 class Main
+  attr_reader :desk
 
 	def initialize
     @ui = Interface.new
@@ -13,6 +15,19 @@ class Main
     @player.bank = Bank.new(@player)
     @computer.bank = Bank.new(@computer)
     @ui.puts_welcome(@player.name)
+  end
+
+  def start_new_game
+    loop do
+      @desk = Deck.new
+      if @player.cards
+        @player.return_cards
+        @computer.return_cards
+      end
+      @player.take_card(@desk.get_start_cards)
+      @computer.take_card(@desk.get_start_cards)
+      return unless start_game
+    end
   end
 
 end
