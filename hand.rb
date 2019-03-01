789677c78e52ae9a5ddf0b91887407750ed35395
ziminator <aleksander.zimin@gmail.com>
2019-03-01 22:09:28 +0300

    BlackJack

--- hand.rb
+++ hand.rb
@@ -1,3 +1,33 @@
 class Hand
+  def card_sum(cards)
+    values = assign_values(cards)
+    values.each do |value|
+      if values.sum < 21 && value == 1
+        values[values.index(value)] = 11 if values.sum + 10 < 22
+      end
+    end
+    values.sum
+  end
 
+  def assign_values(cards)
+    values = []
+    cards.each do |card|
+      values << if pictures.include?(card.value)
+        10
+      elsif card.value == 'Т'
+        1
+      else
+        card.value
+      end
+    end
+    values
+  end
+
+  def player_cards(cards)
+    (cards.map { |card| "#{card.value}#{card.suit}" }).join(' ')
+  end
+
+  def pictures
+    %w[В Д К]
+  end
 end
--- player.rb
+++ player.rb
@@ -1,11 +1,13 @@
 require_relative 'deck.rb'
 require_relative 'cards.rb'
+require_relative 'hand.rb'
 
 class Player
   attr_reader :name
   attr_accessor :bank, :cards
 
   def initialize(name)
+    @hand = Hand.new
     @name = name
     @bank = 0
   end
@@ -18,43 +20,19 @@ class Player
     end
   end
 
+  def card_sum
+    @hand.card_sum(cards)
+  end
+
+  def player_cards
+    @hand.player_cards(cards)
+  end
+
   def return_cards
     self.cards = nil
   end
 
-  def player_cards
-    (cards.map { |card| "#{card.value}#{card.suit}" }).join(' ')
-  end
-
-  def card_sum
-    values = assign_values
-    values.each do |value|
-      if values.sum < 21 && value == 1
-        values[values.index(value)] = 11 if values.sum + 10 < 22
-      end
-    end
-    values.sum
-  end
-
-  def assign_values
-    values = []
-    cards.each do |card|
-      values << if pictures.include?(card.value)
-        10
-      elsif card.value == 'Т'
-        1
-      else
-        card.value
-      end
-    end
-    values
-  end
-
   def two_cards?
     self.cards.size == 2
-  end
-
-  def pictures
-    %w[В Д К]
   end
 end
